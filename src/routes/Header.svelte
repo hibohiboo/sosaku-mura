4336a0ffdf2d4333d1340e7468406fc9fa8d361b
hibohiboo <swdice66@gmail.com>
2022-10-26 23:59:47 +0900

    fix path AB#208

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -2,6 +2,7 @@
 	import { page } from '$app/stores';
 	import logo from '$lib/images/svelte-logo.svg';
 	import github from '$lib/images/github.svg';
+	import { basePath } from '../constants';
 </script>
 
 <header>
@@ -16,15 +17,13 @@
 			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
 		</svg>
 		<ul>
-			<li class:active={$page.url.pathname === '/'}>
-				<a href="/">Home</a>
+			<li class:active={$page.url.pathname === `${basePath}/`}>
+				<a href={`${basePath}/`}>Home</a>
 			</li>
-			<li class:active={$page.url.pathname === '/about'}>
-				<a href="/about">About</a>
+			<li class:active={$page.url.pathname === `${basePath}/about`}>
+				<a href={`${basePath}/about`}>About</a>
 			</li>
-			<li class:active={$page.url.pathname.startsWith('/sverdle')}>
-				<a href="/sverdle">Sverdle</a>
-			</li>
+
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
-const dev = process.env.NODE_ENV === 'development';
+const dev = process.env.npm_lifecycle_event === 'dev';
+console.log('dev', process.env);
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -12,7 +13,7 @@ const config = {
 			fallback: 'index.html'
 		}),
 		prerender: { entries: ['*'] },
-		paths: { base: '' }, //dev ? '' : '/sosaku-mura' },
+		paths: { base: dev ? '' : '/sosaku-mura' },
 		trailingSlash: 'always'
 	}
 };
