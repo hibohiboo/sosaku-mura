a4e2285360cb38ec8279f6c9258b832ba3afb257
hibohiboo <swdice66@gmail.com>
2022-10-26 23:21:00 +0900

    fix for spa AB#208

--- src/routes/sverdle/how-to-play/+page.svelte
+++ src/routes/sverdle/how-to-play/+page.svelte
@@ -1,7 +1,3 @@
-<svelte:head>
-	<title>How to play Sverdle</title>
-	<meta name="description" content="How to play Sverdle" />
-</svelte:head>
 
 <div class="text-column">
 	<h1>How to play Sverdle</h1>
--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
-
+const dev = process.env.NODE_ENV === 'development';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -8,7 +8,12 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			fallback: 'index.html'
+		}),
+		prerender: { entries: ['*'] },
+		paths: { base: dev ? '' : '/sosaku-mura' },
+		trailingSlash: 'always'
 	}
 };
 
