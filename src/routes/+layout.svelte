b11188e638eb188bd56ececcbce351278d70cf4d
hibohiboo <swdice66@gmail.com>
2022-10-28 00:57:14 +0900

    fix background AB#209

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,58 +1,63 @@
 <script>
-	import Header from './Header.svelte';
-	import './styles.css';
+  import Header from './Header.svelte';
+  import './styles.css';
 </script>
 
 <div class="app">
-	<Header />
+  <Header />
 
-	<main>
-		<slot />
-	</main>
+  <main>
+    <slot />
+  </main>
 
-	<footer>
-		<div>© hibohiboo. All rights.</div>
-		<div>
-			<a href="/"> TOP </a>{' '}
-			<a href="/cartagraph/privacy"> プライバシーポリシー </a>{' '}
-			<a href="/cartagraph/privacy"> 利用規約 </a>
-		</div>
-	</footer>
+  <footer>
+    <div>© hibohiboo. All rights.</div>
+    <div>
+      <a href="/cartagraph/privacy"> プライバシーポリシー </a>{' '}
+      <a href="/cartagraph/privacy"> 利用規約 </a>
+    </div>
+  </footer>
 </div>
 
 <style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
+  .app {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
 
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
+  main {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    padding: 1rem;
+    width: 100%;
+    max-width: 64rem;
+    margin: 0 auto;
+    box-sizing: border-box;
+  }
 
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
+  footer {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding: 12px;
+  }
+  footer > div:first-child {
+    right: auto;
+  }
+  footer > div:last-child {
+    left: auto;
+  }
 
-	footer a {
-		font-weight: bold;
-	}
+  footer a {
+    font-weight: bold;
+  }
 
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
+  @media (min-width: 480px) {
+    footer {
+      padding: 12px 0;
+    }
+  }
 </style>
