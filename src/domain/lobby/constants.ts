7b3142e93652833dadf6126d1377f9c2252efe1c
hibohiboo <swdice66@gmail.com>
2022-11-01 19:29:02 +0900

    add lobby login AB#211

---
+++ src/domain/lobby/constants.ts
@@ -0,0 +1,5 @@
+import { roomPassword } from "../../constants";
+
+export const LOBBY_ROOM_ID = 'xxx';
+export const LOBBY_ROOM_NAME = '創作の村'
+export const LOBBY_ROOM_PASS = roomPassword
--- src/domain/udonarium/room/lobby.ts
+++ src/domain/udonarium/room/lobby.ts
@@ -1,10 +1,10 @@
 
-import { roomPassword } from "../../../constants";
+import { LOBBY_ROOM_ID, LOBBY_ROOM_NAME, LOBBY_ROOM_PASS } from "../../../domain/lobby/constants";
 import { Network } from "../class/core/system";
 import { PeerContext } from "../class/core/system/network/peer-context";
 import { EventName } from "../event/constants";
 
 export const createRoom = async () => {
   const userId = Network.peerContext ? Network.peerContext.userId : PeerContext.generateId();
-  Network.open(userId, 'lob', '創作の村', roomPassword);
+  Network.open(userId, LOBBY_ROOM_ID, LOBBY_ROOM_NAME, LOBBY_ROOM_PASS);
 }
