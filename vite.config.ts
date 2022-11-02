bec273b2e36c4d80f401045bcc4a02f4fb64b542
hibohiboo <swdice66@gmail.com>
2022-11-02 22:43:56 +0900

    fix console delete AB#211

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
+import { terser } from 'rollup-plugin-terser'
+
 import dotenv from 'dotenv'
 dotenv.config()
 const dev = process.env.npm_lifecycle_event === 'dev';
 
 const config: UserConfig = {
-  plugins: [sveltekit()],
+  plugins: [sveltekit(), terser({ compress: { drop_console: true } })],
   define: {
     VITE_DEFINE_BASE_PATH: JSON.stringify(dev ? '' : '/sosaku-mura'),
     VITE_DEFINE_SKYWAY_KEY: JSON.stringify(process.env.DEFINE_SKYWAY_KEY),
